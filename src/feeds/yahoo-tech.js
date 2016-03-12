c4cf9390eb6e72db422b223d231bcf908041abdc
Peng Wang <peng@pengxwang.com>
2016-03-12 10:57:06 -0800

    Initial Yahoo Tech filtering :tada:

---
+++ src/feeds/yahoo-tech.js
@@ -0,0 +1,31 @@
+var fetchFeed = require('../fetch-feed');
+var filterFeed = require('../filter-feed');
+var patterns = require('../util').patterns;
+
+function transformMeta(root) {
+  root.transformContent('title', { to: 'Yahoo Tech (filtered)' });
+}
+
+module.exports = function(config, request, response) {
+  fetchFeed({
+    url: 'https://www.yahoo.com/tech/rss',
+    onResponse: function(resFetch, data) {
+      response.setHeader('Content-Type', resFetch.headers['content-type']);
+      filterFeed({
+        config: config,
+        data: data,
+        findEntry: function(root) { return root.find('item'); },
+        findId: function(entry) { return entry.find('guid'); },
+        transformEntry: function() {},
+        transformMeta: transformMeta,
+        verbose: true,
+        onDone: function(data) {
+          response.end(data);
+        }
+      });
+    },
+    onError: function(e) {
+      response.end(e.message);
+    }
+  });
+};
